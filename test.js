6f5cf35db709cb584e57df6bcdd75679ce1592fd
Sharif Ghazzawi <Sharif.Ghazzawi@gmail.com>
2011-10-01 22:42:26 -0400

    Cleaned up the Directory Structure a Bit

--- test.js
+++ test.js
@@ -1,2 +1,16 @@
 require("NetFlowPacket");
+var dgram=require("dgram");
+var server = dgram.createSocket("udp4");
 
+server.on("message", function (mesg, rinfo) {
+    x=new NetFlowPacket(mesg);
+    console.log(x);
+});
+
+server.on("listening", function () {
+  var address = server.address();
+  console.log("server listening " +
+      address.address + ":" + address.port);
+});
+
+server.bind(2055);
