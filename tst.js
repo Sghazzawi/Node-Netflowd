ab03e1288718c9426941a7c653dc52479cf81ae7
Sharif Ghazzawi <Sharif.Ghazzawi@gmail.com>
2011-10-02 14:30:06 -0400

    Added better exception handling.  Included a quick example with mongodb.

---
+++ tst.js
@@ -0,0 +1,22 @@
+require("netflow");
+var mongo=require("mongodb")
+var client = new mongo.Db('test', new mongo.Server("127.0.0.1", 27017,{}));
+client.open(function (err, p_client){
+if (err!=null){
+   console.log(err);
+}    
+});
+x=new netflow(function(err, packet){
+                if(err==null){
+                client.collection('netflow', function(err, collection){
+                     collection.insert(packet,function(err, count){
+            console.log(count);
+            return;
+        });
+
+                 });
+                }
+                else{
+                   console.log(err);
+                }
+              }).listen(2055);
