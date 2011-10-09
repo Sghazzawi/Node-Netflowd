06af5e8e27f960dc2d402d2cf72b1cc17df14831
Sharif Ghazzawi <Sharif.Ghazzawi@gmail.com>
2011-10-09 11:19:44 -0400

    commit

--- tst.js
+++ tst.js
@@ -3,7 +3,7 @@ var x=new Collector(function (err){
                 if(err!=null){
                    console.log("ERROR ERROR \n"+err);
                 }
-              });
-x.on("listening",function(){console.log("listening");});
-x.on("packet",function(packet){console.log(packet);});
-x.listen(2055);
+              })
+              .on("listening",function(){console.log("listening");})
+              .on("packet",function(packet){console.log(packet);})
+              .listen(2055);
