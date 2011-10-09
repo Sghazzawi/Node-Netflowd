c2d3413f7892b8a5ccf9286c45d7ea8b2940ecdc
Sharif Ghazzawi <Sharif.Ghazzawi@gmail.com>
2011-10-08 20:20:11 -0400

    Refactored code to make it mor compliant to node.js module structure.  Used event emitters for more asynchronous/event based structure

--- tst.js
+++ tst.js
@@ -1,22 +1,9 @@
-require("netflow");
-var mongo=require("mongodb")
-var client = new mongo.Db('test', new mongo.Server("127.0.0.1", 27017,{}));
-client.open(function (err, p_client){
-if (err!=null){
-   console.log(err);
-}    
-});
-x=new netflow(function(err, packet){
-                if(err==null){
-                client.collection('netflow', function(err, collection){
-                     collection.insert(packet,function(err, count){
-            console.log(count);
-            return;
-        });
-
-                 });
-                }
-                else{
+var Collector=require("Netflow");
+var x=new Collector(function (err){
+                if(err!=null){
                    console.log("ERROR ERROR \n"+err);
                 }
-              }).listen(2055);
+              });
+x.on("listening",function(){console.log("listening");});
+x.on("packet",function(packet){console.log(packet);});
+x.listen(2055);
